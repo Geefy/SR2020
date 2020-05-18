0c7949405e8cd711bb9972b443578654bf3cfdfa
Kenneth Folke Larsen <KennethLarsenBusiness@hotmail.com>
2020-05-18 14:10:27 +0200

    Requested push

--- SR2020/Scripts/spa.js
+++ SR2020/Scripts/spa.js
@@ -8,21 +8,23 @@
         })
 
         document.querySelectorAll('nav-link').forEach((link) => {
-            link.addEventListener('click', app.nav)
+            link.addEventListener('click', app.nav);
         })
-        history.replaceState({}, 'Home', '#home')
-        window.addEventListener('popstate', app.poppin)
+        history.replaceState({}, 'Home', '#home');
+        //window.addEventListener('popstate', app.poppin)
+        document.getElementById('btnsure').addEventListener('popstate', app.poppin);
     },
 
     nav: function (ev) {
         ev.preventDefault();
+        let currentPage = ev.target.getAttribute('data-target');
     },
 
     pageShown: function (ev) {
     },
 
     poppin: function (ev) {
-        console.log(location.hash, 'popstate event')
+        console.log(location.hash, 'helloworld')
     }
 }
 
