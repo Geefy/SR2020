ad9efdb5de2beccdea74a13c7285a442decfd914
Marc Andersen <marchot1999@gmail.com>
2020-05-19 09:36:05 +0200

    Case layout "Markup" made

---
+++ SR2020/Scripts/spa.js
@@ -0,0 +1,29 @@
+﻿const app = {
+    pages: [],
+    show: new Event('show'),
+    init: function () {
+        app.pages = document.querySelectorAll('.page');
+        app.pages.forEach((pg) => {
+            pg.addEventListener('show', app.pageShown);
+        })
+
+        document.querySelectorAll('nav-link').forEach((link) => {
+            link.addEventListener('click', app.nav)
+        })
+        history.replaceState({}, 'Home', '#home')
+        window.addEventListener('popstate', app.poppin)
+    },
+
+    nav: function (ev) {
+        ev.preventDefault();
+    },
+
+    pageShown: function (ev) {
+    },
+
+    poppin: function (ev) {
+        console.log(location.hash, 'popstate event')
+    }
+}
+
+document.addEventListener('DOMContentLoaded', app.init);
