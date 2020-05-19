1871d2d5c9794d38e1a392f814e686861ca13285
Marc Andersen <marchot1999@gmail.com>
2020-05-19 13:05:06 +0200

    Javascript to create case from data

--- SR2020/Scripts/JSCase.js
+++ SR2020/Scripts/JSCase.js
@@ -1,3 +1,26 @@
-﻿function myFunction() {
-    var caseDiv = document.createElement("div");
-    document.body.appendChild(caseDiv);
+﻿function CreateContainer() {
+    var div = document.createElement('div');
+    div.className = 'container';
+    div.id = 'ContainerEdit';
+    document.getElementById("MainBody").appendChild(div);
+}
+
+function CreateCase() {
+    var caseContainer = document.createElement('div');
+    caseContainer.className = 'CaseContainer';
+    caseContainer.id = '1';
+    var colorBox = document.createElement('div');
+    colorBox.className = 'ColorBox';
+    colorBox.id = 'Green';
+    var infoBox = document.createElement('div');
+    infoBox.className = 'InfoBox';
+    infoBox.id = 'infoBoxTest'
+    var InfoText = document.createElement('p');
+    InfoText.innerHTML = "Text";
+    InfoText.className = 'InfoBoxText';
+    InfoText.id = 'test';
+    document.getElementById('ContainerEdit').appendChild(caseContainer);
+    document.getElementById('1').appendChild(colorBox);
+    document.getElementById('1').appendChild(infoBox);
+    document.getElementById('infoBoxTest').appendChild(InfoText);
+}
