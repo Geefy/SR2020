63ca3c9abff095be1414fc096a31ad79127e12ea
Kenneth Folke Larsen <KennethLarsenBusiness@hotmail.com>
2020-05-20 12:19:27 +0200

    ApiCall Works

--- SR2020/Scripts/ApiCall.js
+++ SR2020/Scripts/ApiCall.js
@@ -1,13 +1,4 @@
-﻿const ul = document.getElementById('authors');
-const url = 'https://randomuser.me/api/?results=1';
-
-function createNode(element) {
-    return document.createElement(element);
-}
-
-function append(parent, el) {
-    return parent.appendChild(el);
-}
+﻿const url = 'https://randomuser.me/api/?results=1';
 
 
 fetch(url) //set the url of the api
@@ -20,11 +11,23 @@ fetch(url) //set the url of the api
                 span = createNode('span');
             img.src = author.picture.medium;
             span.innerHTML = `${author.name.first} ${author.name.last}`;
+            var ul = document.getElementById('WeAreTesting');
             append(li, img);
+            console.log(img);
             append(li, span);
+            console.log(span);
+            console.log(ul);
             append(ul, li);
         })
     })
     .catch(function (error) {
         console.log(error);
     })
+
+function createNode(element) {
+    return document.createElement(element);
+}
+
+function append(parent, el) {
+    return parent.appendChild(el);
+}
