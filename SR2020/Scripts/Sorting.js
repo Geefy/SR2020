58a6be953b788da4903470cf16ef8df7e61aa040
Marc Andersen <marchot1999@gmail.com>
2020-06-08 12:28:54 +0200

    timer and sorting Test

---
+++ SR2020/Scripts/Sorting.js
@@ -0,0 +1,4 @@
+﻿var kids = ["Adam", "Benjamin", "Gertrod", "Glen", "Dorte"];
+
+kids.sort();
+console.log(kids);
---
+++ SR2020/Scripts/TimerTest.js
@@ -0,0 +1,16 @@
+﻿var countDownDate = new Date("Jun 4, 2020 10:04:00").getTime();
+
+var myfunc = setInterval(function () {
+
+    var now = new Date().getTime();
+    var timeleft = countDownDate - now;
+
+    var days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
+
+    console.log(minutes + "m ");
+    console.log(seconds + "s ");
+
+}, 1000)
