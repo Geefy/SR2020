80900208c55fbfd5b0cc9e455afbd9b7f25b42b4
Kenneth Folke Larsen <KennethLarsenBusiness@hotmail.com>
2020-06-09 10:42:17 +0200

    Identity Is now semi-working

--- SR2020/Index.aspx.cs
+++ SR2020/Index.aspx.cs
@@ -3,7 +3,9 @@ using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.Owin.Security;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -12,6 +14,11 @@ namespace SR2020
 {
     public partial class WebForm1 : System.Web.UI.Page
     {
+        struct UserCredentials
+        {
+            public string Username;
+            public string Password;
+        }
         protected void Page_Load(object sender, EventArgs e)
         {
             LoginButton.Text = "To SPA";
@@ -19,25 +26,26 @@ namespace SR2020
 
         protected void ToSpa(object sender, EventArgs e)
         {
-            //var userStore = new UserStore<IdentityUser>();
-            //var manager = new UserManager<IdentityUser>(userStore);
-            //var user = new IdentityUser() { UserName = "Marc" };
+            try
+            {
+                using (var client = new System.Net.Http.HttpClient())
+                {
+                    client.BaseAddress = new Uri("https://localhost:44350/");
+                    client.DefaultRequestHeaders.Accept.Clear();
+                    client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+                    var userLogin = new UserCredentials() { Username = Uname.Value, Password = Pword.Value };
+                    var response = client.PostAsJsonAsync("api/auth/Login", userLogin).Result;
+                    if (response.IsSuccessStatusCode)
+                        Response.Redirect(@"Spa.html");
+                        
+                }
 
-            //IdentityResult result = manager.Create(user, "SoLovely");
+            }
+            catch (Exception ds)
+            {
 
-            //if (result.Succeeded)
-            //{
-            //    var authenticationManager = HttpContext.Current.GetOwinContext().Authentication;
-            //    var userIdentity = manager.CreateIdentity(user, DefaultAuthenticationTypes.ApplicationCookie);
-            //    authenticationManager.SignIn(new AuthenticationProperties() { }, userIdentity);
-            //    Response.Redirect("~/Login.aspx");
-            //}
-            //else
-            //{
-            //    StatusMessage.Text = result.Errors.FirstOrDefault();
-            //}
-
-            Response.Redirect(@"Spa.html");
+                Debug.WriteLine(ds.Message);
+            }
         }
     }
 }
